2de9675b25150571a30121b6ebef82216e551649
Christian Diaz <chrisdiaz3210@gmail.com>
2022-09-28 01:47:43 -0700

    Added Other Irrational Numbers but the High Score System broke

--- main.py
+++ main.py
@@ -4,46 +4,71 @@ import json
 try:
     highScoreHTable = json.load("High_Scores.json")
 except:
+    print("ran")
     with open("High_Scores.json", 'w') as file:
         file.write("{\n}")
     highScoreHTable = dict()
-def getMoreNumbers(place: int):
-    with open("PI_DIGITS.txt", 'r') as file:
-        file.seek(place*INCREMENT)
+
+
+def getMoreNumbers(place: int, txtFileName: str):
+    # TODO find a way to generate an infinite amount of numbers and not just the 1000000 that are in the text file
+    with open(txtFileName, 'r') as file:
+        file.seek(place * INCREMENT)
         PIString = file.read(INCREMENT)
         PIList = list(PIString)
         return PIList
+
+
 def CMDmain():
+    print("Please input the irrational number you'd like to memorize:")
+    with open("Irrationals.json", "r") as file:
+        irrationalHTable = json.load(file)
+    for key, val in irrationalHTable.items():
+        print(f"{key}. {val}")
+    while(True):
+        num = input("Please input the number(int) you want to practice\n")
+        try:
+            numBoolean = num in irrationalHTable.keys()
+        except:
+            continue
+        if(numBoolean):
+            break
+    with open("IrrationalFileNames.json", "r") as file:
+        IrrationalFilesTexts = json.load(file)
+    txtFileName = IrrationalFilesTexts[num]
     place = 0
-    numbers = getMoreNumbers(place)
+    numbers = getMoreNumbers(place, txtFileName)
     failedBool = True
     try:
-        highScore = highScoreHTable["PI_DIGITS.txt"]
+        highScore = highScoreHTable[txtFileName]
     except:
-        highScoreHTable["PI_DIGITS.txt"] = 0
-        highScore = highScoreHTable["PI_DIGITS.txt"]
-    while(True):
+        highScoreHTable[txtFileName] = 0
+        highScore = highScoreHTable[txtFileName]
+    while (True):
         count = 0
         for number in numbers:
             userNumber = input()
-            if(userNumber == number):
+            if (userNumber == number):
                 count += 1
                 continue
             else:
                 failedBool = False
                 break
-        if(failedBool == False):
+        if (failedBool == False):
             break
         else:
             place += 1
-            numbers = getMoreNumbers(place)
+            numbers = getMoreNumbers(place, txtFileName)
     print(f"Score: {count}")
-    if(count>highScore):
-        highScoreHTable["PI_DIGITS.txt"] = count
+    if (count > highScore):
+        highScoreHTable[txtFileName] = count
+        print(highScoreHTable)
         with open("High_Scores.json", 'w') as file:
             json.dump(highScoreHTable, file)
         print("New High Score")
+
+
 CMDmain()
-#TODO Unit Tests
-#TODO GUI
-#TODO Other Irrational Numbers
+# TODO Unit Tests
+# TODO GUI
+# TODO Other Irrational Numbers
